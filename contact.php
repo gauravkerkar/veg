80537ea1d081698ac02075325b83bc8f9ecb9235
Vaman <sec.acc901@gmail.com>
2018-12-27 14:11:18 +0530

    Some changes

---
+++ contact.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!--Cascading Stylesheet File-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!--Bootstrap 4 CDN Link-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-sm-6 bg-warning">
+            <section class="text-white contact-template">
+                <h1 class="text-center">CONTACT US</h1>
+                <p class="lead">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+            </section>
+        </div>
+        <div class="col-sm-6">
+            <section class="message-template">
+                <h1>Message</h1>
+                <br><br>
+                <div class="form-group">
+                    <input type="text" name="name" class="form-control" placeholder="Name">
+                </div>
+                <div class="form-group">
+                    <input type="email" name="email" class="form-control" placeholder="E-mail address">
+                </div>
+                <div class="form-group">
+                    <textarea class="form-control" rows="5" placeholder="Comment"></textarea>
+                </div>
+                <button type="button" class="btn btn-primary btn-block">Submit</button>
+            </section>
+        </div>
+    </div>
+</body>
+
+</html>
